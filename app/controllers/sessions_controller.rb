f42b86ef52cf1c8c8165a8e45d452b4b91f31d4f
johncodesgood <bs19087@yahoo.com>
2014-11-19 18:33:48 -0500

    added user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
+require 'pry'
 class SessionsController < ApplicationController
   def new
   end
 
   def create
-    user = User.find_by_email(params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by_name(params[:name])
+    if user && user.authenticate(params[:password])
+      redirect_to user
+    else
+      render 'new'
     end
   end
 
