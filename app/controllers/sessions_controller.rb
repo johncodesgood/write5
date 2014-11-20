ab5347060517bb0771da2d0ace8158085e45c823
johncodesgood <bs19087@yahoo.com>
2014-11-19 20:11:19 -0500

    user login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_name(params[:name])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_name(params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to user
     else
       render 'new'
     end
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_path
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class UsersController < ApplicationController
  
   def new
@@ -5,9 +7,10 @@ class UsersController < ApplicationController
   end
 
   def create
-  @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
+  user = User.new(user_params)
+    if user.save
+      log_in user
+      redirect_to user
     else
       render 'new'
     end
