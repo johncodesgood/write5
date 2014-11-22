052456ec79cb533654e232ace6b9c5487da1dc5f
johncodesgood <bs19087@yahoo.com>
2014-11-22 13:47:27 -0500

    removed facebook auth table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122155429) do
+ActiveRecord::Schema.define(version: 20141121164326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 20141122155429) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-  end
-
-  create_table "facebook_oauth_settings", force: true do |t|
-    t.string   "access_token"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
