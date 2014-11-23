3258a46538be238ea5dae07cbdc0156def41076d
amadden80 <amadden80@gmail.com>
2014-11-23 13:00:41 -0500

    image-path to load image assets

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141121164326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,11 +22,6 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-  end
-
-  create_table "sessions", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
@@ -46,6 +40,5 @@ ActiveRecord::Schema.define(version: 0) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
 
 end
