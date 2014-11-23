6c6c278bf76ccd6e45fec3b8603867a982a02f7a
maninigupta <manini.gupta@gmail.com>
2014-11-22 19:25:21 -0500

    incorporate AccountabilityKeeper into shaming check.rb

--- app/models/check.rb
+++ app/models/check.rb
@@ -5,23 +5,27 @@ class Check < ActiveRecord::Base
   def self.weekly
     puts "Daily check"
     t = Time.new
-    if t.thursday?
-      puts "It's Sunday!!!"
-      week_begin = Date.today.at_beginning_of_week
-      week_end = Date.today.at_end_of_week
-      users = User.all
-      users.each do |user|
-        count = 0
-        user.articles.each do |article|
-          if (article.created_at >= week_begin) && (article.text.split.size >= user.word_count.to_i)
-            count += 1
-          end          
+    return unless t.thursday?
+
+    puts "It's Sunday!!!"
+    week_begin = Date.today.at_beginning_of_week
+    week_end = Date.today.at_end_of_week
+    users = User.all
+    users.each do |user|
+      count = 0
+      # sample wishful code: users.articles_written_this_week.count >= 5
+      user.articles.each do |article| # TODO only count articles from this week
+        if (article.created_at >= week_begin) && (article.text.split.size >= user.word_count.to_i)
+          count += 1
         end
-        if count >= 5
-          puts "#{user.name} no shaming"
-        else
-          puts "#{user.name} shaming"
-        end
+      end
+
+      if count >= 5
+        puts "#{user.name} no shaming"
+        puts AccountabilityKeeper.new(user).text_friend "no shaming"
+      else
+        puts "#{user.name} shaming"
+        puts AccountabilityKeeper.new(user).text_friend "shaming"
       end
     end
 
