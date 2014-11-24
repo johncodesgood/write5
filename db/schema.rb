1e474ec33ef5434c45caedcb8025d0a4352faf0b
RadWells <scwells@icloud.com>
2014-11-24 10:47:07 -0500

    Font and layout tweeks

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141121164326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -46,6 +45,5 @@ ActiveRecord::Schema.define(version: 0) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-
 
 end
