9731f3a8c6c3ae5a09a0cb4fc89512311df9422f
johncodesgood <bs19087@yahoo.com>
2014-11-23 23:17:52 -0500

    fix for twilio

--- app/models/accountability_keeper.rb
+++ app/models/accountability_keeper.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class AccountabilityKeeper < ActiveRecord::Base
   belongs_to :user
 
@@ -9,17 +11,29 @@ class AccountabilityKeeper < ActiveRecord::Base
     # ENV["account_sid"]
     # ENV["auth_token"]
     @user = user
-    account_sid = 'ACcaae044553dde80112b45a44a14d0ac5'
-    auth_token = '425eee6044e863987cc2c7f73ddee8f5'
+    # account_sid = 'ACcaae044553dde80112b45a44a14d0ac5'
+    # auth_token = '425eee6044e863987cc2c7f73ddee8f5'
+    account_sid = 'ACfe71131fcea1a86e22c44ff95a779d1a' 
+    auth_token = 'c270faac49003d2d76f084028a02a0af'
 
-    @client = Twilio::REST::Client.new account_sid, auth_token
+    @client = Twilio::REST::Client.new account_sid, auth_token 
+
+    phone = @user.accountability_keeper.accountability_keeper
 
     @client.account.messages.create({
-      :from => '+15519996336',
-      # :to => "#{@user.accountability_keeper.accountability_keeper.to_i}",
-      :to => '+1#{@user.accountability_keeper}',
-      :body => "Remember when #{@user.name} was supposed to finish writing some stuff this week? You should look into that...",
+        :from => '+15519996336',
+        :to => phone, 
+        :body => "Remember when #{@user.name} was supposed to finish writing some stuff this week? You should look into that...",   
     })
+
+      #  :from => '+14846853890', 
+    # @client.account.messages.create({
+    #   # :from => '+15519996336',
+    #   :from => '+14846853890',
+    #   # :to => "#{@user.accountability_keeper.accountability_keeper.to_i}",
+    #   :to => "+1#{@user.accountability_keeper.accountability_keeper}",
+    #   :body => "Remember when #{@user.name} was supposed to finish writing some stuff this week? You should look into that...",
+    # })
 
   end
 end
